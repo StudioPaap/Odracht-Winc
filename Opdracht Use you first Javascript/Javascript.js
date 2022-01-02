b947c73665215f8639620f68997e2a9fac178bd0
Studio Paap <59624133+StudioPaap@users.noreply.github.com>
2022-01-02 17:30:47 +0100

    Add files via upload

---
+++ Opdracht Use you first Javascript/Javascript.js
@@ -0,0 +1,61 @@
+console.log(name);
+undefined
+let name = "Anouk";
+let age = 30;
+undefined
+console.log(name);
+undefined
+console.log(name);
+undefined
+let name = 'Mick';
+undefined
+console.log(name);
+undefined
+let name = "nico"
+undefined
+console.log(name)
+undefined
+let name = 'Nico'
+undefined
+let name = 'Nico'
+undefined
+console.log("hello Winc Acadmy");
+undefined
+let name = "Anouk"
+undefined
+let name = 'Anouk';
+undefined
+console.log(name);
+undefined
+"4" + "4"
+'44'
+if (name = "nico") {
+    alert('Hello Nico');
+}
+undefined
+let name = "Anouk";
+undefined
+let name = "nico";
+undefined
+if (name = "Anouk"); {
+    alert("oprotten");
+}
+undefined
+let age = 30;
+let age = 30;
+let age = 30;
+let age = 30;
+undefined
+undefined
+undefined
+undefined
+typeof age
+'number'
+let age = "dertig";
+undefined
+typeof age
+'string'
+12 + 2
+14
+100 / 2
+50

8322a804693f5e9db6efd8cf650ea1291a0e1c0d
Studio Paap <59624133+StudioPaap@users.noreply.github.com>
2022-01-09 13:43:17 +0100

    Add files via upload

---
+++ Opdracht-Use-if-else/Index.js
@@ -0,0 +1,29 @@
+//Check voor leeftijd
+const age = 12;
+
+if (age >= 18) {
+console.log("Je bent ouder dan 18 jaar, je bent welkom");}
+else{
+console.log ("Je bent te jong en niet welkom hier.");}
+
+
+
+//Check voor ladiesnight
+let isFemale = true;
+
+if (isFemale = true){
+    console.log ("Het is ladiesnight je bent welkom!")
+}
+else{
+    console.log("Het is ladiesnight, je bent niet welkom")
+}
+
+
+//Check of iemand safe kan rijden
+let driverStatus = "designated";
+
+if (driverStatus= "designated"){
+    console.log("Je mag rijden, het is veilig")}
+else {
+    console.log("Zoek iemand die de bob wil zijn voor je of pak het ov")
+}

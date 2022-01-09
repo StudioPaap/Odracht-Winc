5720624a4f979fff9ffdef4e9a1d30fe3dca2bc0
Studio Paap <59624133+StudioPaap@users.noreply.github.com>
2022-01-09 17:52:45 +0100

    Add files via upload

---
+++ Opdracht_functions/index.js
@@ -0,0 +1,29 @@
+//Defining a function
+const makePizza = function(){
+console.log("kneed het deeg");
+console.log("doe er pastasaus over");
+console.log("voeg groenstes en kaas toe");
+console.log("bak de pizza op 200 graden");
+};
+
+const makeSushi = function(){
+    console.log("kook de rijst");
+    console.log("leg de zeewiervellen klaar op de mat");
+    console.log("schep een lepel rijst");
+    console.log("voek een strook komkommer toe");
+    console.log("rol de sushi op met de mat en snijd in stukken");
+    };
+
+ 
+//calling the function
+
+
+makeSushi();
+
+makeSushi();
+
+makeSushi();
+
+makePizza();
+
+makePizza();
